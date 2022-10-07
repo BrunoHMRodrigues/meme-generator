e19b9f584b3fee83b4a669f36f6d23f5e2108b79
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-10-07 19:20:42 -0300

    Realizado Requisito Obrigatório 02

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ function copyText(event) {
     textPreviewMeme.innerText = getTextValue;
 }
 inputBox.addEventListener("keyup", copyText);
+
+const buttonImage = document.getElementById("meme-insert");
+
+function insertImagePreview () {
+     const getImage = new FileReader();
+
+     getImage.addEventListener("load", () => {
+         const uploadedImage = getImage.result;
+         const sectionPreview = document.getElementById("meme-image");
+         sectionPreview.style.backgroundImage = `url(${uploadedImage})`;
+     });
+     getImage.readAsDataURL(this.files[0]);    
+}
+buttonImage.addEventListener("change", insertImagePreview);
