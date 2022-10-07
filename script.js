b92811769013c45a887afd3d1bb2eec31374abe6
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-10-07 20:23:01 -0300

    Realizado Requisito Bônus 06

--- script.js
+++ script.js
@@ -21,3 +21,30 @@ function insertImagePreview () {
      getImage.readAsDataURL(this.files[0]);    
 }
 buttonImage.addEventListener("change", insertImagePreview);
+
+const buttonFire = document.getElementById("fire");
+
+function borderFire (event) {
+    const memeDisplay = document.getElementById("meme-image-container");
+    
+    memeDisplay.className = "fire";
+}
+buttonFire.addEventListener("click", borderFire);
+
+const buttonWater = document.getElementById("water");
+
+function borderWater (event) {
+    const memeDisplay = document.getElementById("meme-image-container");
+    
+    memeDisplay.className = "water";
+}
+buttonWater.addEventListener("click", borderWater);
+
+const buttonEarth = document.getElementById("earth");
+
+function borderEarth (event) {
+    const memeDisplay = document.getElementById("meme-image-container");
+
+    memeDisplay.className = "earth";
+}
+buttonEarth.addEventListener("click", borderEarth);
